f264078aefdb63c4859fda4483cb378a54b147e0
Momcilo <momcilopetkovic27@gmail.com>
2025-03-05 01:44:44 +0100

    Implemented and using ModelMapper Library

--- todo-management/src/main/java/com/momcilocode/todo_management/service/impl/TodoServiceImpl.java
+++ todo-management/src/main/java/com/momcilocode/todo_management/service/impl/TodoServiceImpl.java
@@ -4,7 +4,8 @@ import com.momcilocode.todo_management.dto.TodoDto;
 import com.momcilocode.todo_management.entity.Todo;
 import com.momcilocode.todo_management.repository.TodoRepository;
 import com.momcilocode.todo_management.service.TodoService;
-import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,25 +13,39 @@ public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
 
+    private ModelMapper modelMapper;
+
     public TodoServiceImpl(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
+    @Autowired
+    public TodoServiceImpl(TodoRepository todoRepository, ModelMapper modelMapper) {
+        this.todoRepository = todoRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
         //convert TodoDto to Todo JPA Entity
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.isCompleted());
+//        Todo todo = new Todo();
+//        todo.setTitle(todoDto.getTitle());
+//        todo.setDescription(todoDto.getDescription());
+//        todo.setCompleted(todoDto.isCompleted());
+
+        Todo todo = modelMapper.map(todoDto, Todo.class);
+
         //Todo Jpa Entity
         Todo savedTodo = todoRepository.save(todo);
         //Convert savedTodo jpa entity object into TodoDto object
-        TodoDto savedTodoDto = new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+//        TodoDto savedTodoDto = new TodoDto();
+//        savedTodoDto.setId(savedTodo.getId());
+//        savedTodoDto.setTitle(savedTodo.getTitle());
+//        savedTodoDto.setDescription(savedTodo.getDescription());
+//        savedTodoDto.setCompleted(savedTodo.isCompleted());
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
         return savedTodoDto;
     }
 }
