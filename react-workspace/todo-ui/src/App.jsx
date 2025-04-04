8a2353f4e0d42f29984ab9acddca547e572415ae
Momcilo-Petkovic <momcilopetkovic27@gmail.com>
2025-04-04 22:23:25 +0200

    Some Todo frontend

---
+++ react-workspace/todo-ui/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ListTodoComponent from './components/ListTodoComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <ListTodoComponent/>
+    </>
+  )
+}
+
+export default App
